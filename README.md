44603261145af389f6748703ef46ed5aef0184e5
Gamearoo Codes <gamearoo10@gmail.com>
2022-03-20 19:04:47 -0400

    added am/pm

--- README.md
+++ README.md
@@ -1,18 +1,31 @@
 # Simply-Logger
+
 > A simple logger using Chalk 4.1.0 and Moment.
+
 ## Utilisation & Example
+
 > To install it, run the following command:
+
 ```batch
 npm i simply-logger
 ```
 
+> Update
+
+```js
+const myLogger = new simplylogger.Logger("MyLogger", "Europe/Paris");
+//to
+const myLogger = new simplylogger.Logger("MyLogger", "Europe/Paris", 12); //12 or 24  12 = Am/ PM 24 = 24 hour clock
+```
+
 > Code example:
+
 ```js
 // don't forget to require the module!
-const simplylogger = require('simply-logger');
+const simplylogger = require("simply-logger");
 
 // creates the logger
-const myLogger = new simplylogger.Logger("MyLogger", "Europe/Paris");
+const myLogger = new simplylogger.Logger("MyLogger", "Europe/Paris", 24); //change 24 to 12 for AM/PM
 
 /* examples */
 
@@ -27,6 +40,7 @@ myLogger.error("Oh sh*t... an error...");
 ```
 
 ## Additionnal Informations
+
 > This package is under GNU Public Licence 3.0.
 > Use the packages as your wishes.
 > If you find any bugs, please repport them at https://github.com/TheDogHusky/simply-logger/issues
--- index.js
+++ index.js
@@ -1,52 +1,75 @@
-'use-strict';
+"use-strict";
 
-const chalk = require('chalk');
-const moment = require('moment');
-const tz = require('moment-timezone')
+const chalk = require("chalk");
+const moment = require("moment");
+const tz = require("moment-timezone");
 
 //The valid timezone function
 function isValidTimeZone(tz) {
-    if (!Intl || !Intl.DateTimeFormat().resolvedOptions().timeZone) {
-        throw new Error('Time zones are not available in this environment');
-    }
+	if (!Intl || !Intl.DateTimeFormat().resolvedOptions().timeZone) {
+		throw new Error("Time zones are not available in this environment");
+	}
 
-    try {
-        Intl.DateTimeFormat(undefined, {timeZone: tz});
-        return true;
-    }
-    catch (ex) {
-        return false;
-    };
-};
-
+	try {
+		Intl.DateTimeFormat(undefined, { timeZone: tz });
+		return true;
+	} catch (ex) {
+		return false;
+	}
+}
 
 //let's initialize the logger!
 class Logger {
-    constructor(name, timezone) {
-        this.name = name;
-        this.timezone = timezone;
+	constructor(name, timezone, format) {
+		//use ("name", "timezone", "format")
+		this.name = name;
+		this.timezone = timezone;
+		this.format = Number(format); // 12 or 24
 
-        if(!isValidTimeZone(String(timezone))) throw new Error(`The timezone ${timezone} is invalid.`);
-    }
-    
-    info(text) {
-        var d = new Date();
-        var date = moment(d).tz(String(this.timezone)).format("HH:mm:ss");
-        console.log(`${chalk.cyan(date)}${chalk.gray(` - `)}${chalk.blue("[")}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.green("Info")} ${chalk.gray("▪")} ${chalk.greenBright(text)}`);
-    }
-    
-    warn(text) {
-        var d = new Date();
-        var date = moment(d).tz(String(this.timezone)).format("HH:mm:ss");
-        console.log(`${chalk.cyan(date)}${chalk.gray(` - `)}${chalk.blue("[")}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.yellow("Warn")} ${chalk.gray("▪")} ${chalk.yellowBright(text)}`);
-    }
-    
-    error(text) {
-        var d = new Date();
-        var date = moment(d).tz(String(this.timezone)).format("HH:mm:ss");
-        console.log(`${chalk.cyan(date)}${chalk.gray(` - `)}${chalk.blue("[")}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.red("Error")} ${chalk.gray("▪")} ${chalk.redBright(text)}`);
-    }
-};
+		if (!isValidTimeZone(String(timezone)))
+			throw new Error(`The timezone ${timezone} is invalid.`);
+		var d = new Date();
+		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
+		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
+
+		let date = date24;
+
+		if (this.format === 12) date = date12;
+		if (this.format === 24) date = date24;
+
+		this.date = date;
+	}
+
+	info(text) {
+		console.log(
+			`${chalk.cyan(this.date)}${chalk.gray(` - `)}${chalk.blue(
+				"["
+			)}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.green(
+				"Info"
+			)} ${chalk.gray("▪")} ${chalk.greenBright(text)}`
+		);
+	}
+
+	warn(text) {
+		console.log(
+			`${chalk.cyan(this.date)}${chalk.gray(` - `)}${chalk.blue(
+				"["
+			)}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.yellow(
+				"Warn"
+			)} ${chalk.gray("▪")} ${chalk.yellowBright(text)}`
+		);
+	}
+
+	error(text) {
+		console.log(
+			`${chalk.cyan(this.date)}${chalk.gray(` - `)}${chalk.blue(
+				"["
+			)}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.red(
+				"Error"
+			)} ${chalk.gray("▪")} ${chalk.redBright(text)}`
+		);
+	}
+}
 
 //exporting
 module.exports.Logger = Logger;

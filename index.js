d67277301f3b6deba6d953972db6d7db478fc7a9
TheDogHusky <classy.craft3r@gmail.com>
2022-03-23 19:25:59 +0100

    Added examples to the docs

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 "use-strict";
 
+/**
+ * @name Simply-Logger
+ * @author ClassyCrafter
+ * @version 0.3.25
+ * @license GNU-3.0
+ */
+
 const chalk = require("chalk");
 const moment = require("moment");
 const tz = require("moment-timezone");
@@ -27,6 +34,10 @@ class Logger {
 	 * @param {String} name The name of the Logger. Will be logged as [<name>]
 	 * @param {String} timezone The timezone of the Logger. See https://gist.github.com/diogocapela/12c6617fc87607d11fd62d2a4f42b02a for a full list of timezones.
 	 * @param {Number} format The hour format of the Logger. It's a Number, put 12 or 24. 12 = AM/PM hour format. 24 = 24hours format.
+	 * @example
+	 * const { Logger } = require('simply-logger');
+	 * 
+	 * const myLogger = new Logger("MyLogger", "Europe/Paris", 24);
 	 */
 	constructor(name, timezone, format = 24) {
 		//use ("name", "timezone", "format")

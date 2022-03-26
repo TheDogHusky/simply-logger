a5b7d057b50fba0e0f2636cb36529608488b4398
TheDogHusky <classy.craft3r@gmail.com>
2022-03-26 21:33:25 +0100

    Added the system again

--- index.js
+++ index.js
@@ -10,11 +10,12 @@
 const chalk = require("chalk");
 const moment = require("moment");
 const tz = require("moment-timezone");
-//const fs = require('fs');
+const fs = require('fs');
+const path = require('path');
 
 //The valid timezone function
 /**
- *
+ * 
  * @param {String} tz The timezone to verify
  * @returns Boolean true or false
  */
@@ -36,133 +37,131 @@ function isValidTimeZone(tz) {
  */
 class Logger {
 	/**
-	 *
+	 * 
 	 * @param {String} name The name of the Logger. Will be logged as [name]
 	 * @param {String} timezone The timezone of the Logger. See https://gist.github.com/diogocapela/12c6617fc87607d11fd62d2a4f42b02a for a full list of timezones.
 	 * @param {Number} format The hour format of the Logger. It's a Number, put 12 or 24. 12 = AM/PM hour format. 24 = 24hours format.
-	 * @param {String} dir The directory to create logs in
+	 * @param {String} path The path to the directory to create logs in
+	 * @param {String} writeLogs If the Logger writes logs in a file
 	 * @example const { Logger } = require('simply-logger');
-	 *
+	 * 
 	 * const myLogger = new Logger("MyLogger", "Europe/Paris", 24, "./logs");
 	 */
-	constructor(name, timezone, format = 24 /*, dir = null for future update*/) {
+	constructor(name, timezone, format = 24, dirpath = null, writeLogs = false) {
 		//use ("name", "timezone", "format")
 		this.name = name;
 		this.timezone = timezone;
 		this.format = Number(format); // 12 or 24
-		//this.dir = dir; - Future Update
-
+		this.dirpath = dirpath;
+		this.writeLogs = writeLogs;
 		if (!isValidTimeZone(String(timezone)))
 			throw new Error(`The timezone ${timezone} is invalid.`);
+		var d = new Date();
+		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
+		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
+
+		let date = date24;
+
+		if (this.format === 12) date = date12;
+		if (this.format === 24) date = date24;
+
+		this.date = date;
+		if(this.writeLogs === false) return;
+		if(this.dirpath === null) return;
+		if(!fs.existsSync(this.dirpath)) throw new Error("The specified path does not exists.");
+		this.filepath = path.join(this.dirpath, `${this.name}.log`);
 	}
 	/**
-	 *
+	 * 
 	 * @param {String} text The text to log as an info.
 	 */
 	info(text) {
-		var d = new Date();
-		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
-		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
-
-		let date = date24;
-
-		if (this.format === 12) date = date12;
-		if (this.format === 24) date = date24;
 		console.log(
-			`${chalk.cyan(date)}${chalk.gray(` - `)}${chalk.blue(
-				"["
-			)}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.green(
-				"Info"
-			)} ${chalk.gray("▪")} ${chalk.greenBright(text)}`
+			`${chalk.cyan(this.date)}${chalk.gray(` - `)}${chalk.blue("[")}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.green("Info")} ${chalk.gray("▪")} ${chalk.greenBright(text)}`
 		);
+		if(this.writeLogs === false) return;
+		const formattedMessage = `${this.date} - [${this.name}] Info ▪ ${text}`
+		fs.appendFile(this.filepath, formattedMessage + '\n', (err) => {
+			if (err) {
+				throw new Error(err)
+			}
+		});
 	}
 	/**
-	 *
+	 * 
 	 * @param {String} text The text to log as a warn.
 	 */
 	warn(text) {
-		var d = new Date();
-		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
-		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
-
-		let date = date24;
-
-		if (this.format === 12) date = date12;
-		if (this.format === 24) date = date24;
 		console.log(
-			`${chalk.cyan(date)}${chalk.gray(` - `)}${chalk.blue(
-				"["
-			)}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.yellow(
-				"Warn"
-			)} ${chalk.gray("▪")} ${chalk.yellowBright(text)}`
+			`${chalk.cyan(this.date)}${chalk.gray(` - `)}${chalk.blue("[")}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.yellow("Warn")} ${chalk.gray("▪")} ${chalk.yellowBright(text)}`
 		);
+		if(this.writeLogs === false) return;
+		const formattedMessage = `${this.date} - [${this.name}] Warn ▪ ${text}`
+		fs.appendFile(this.filepath, formattedMessage + '\n', (err) => {
+			if (err) {
+				throw new Error(err)
+			}
+		});
 	}
 	/**
-	 *
+	 * 
 	 * @param {String} text The text to log as an error.
 	 */
 	error(text) {
-		var d = new Date();
-		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
-		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
-
-		let date = date24;
-
-		if (this.format === 12) date = date12;
-		if (this.format === 24) date = date24;
 		console.log(
-			`${chalk.cyan(date)}${chalk.gray(` - `)}${chalk.blue(
-				"["
-			)}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.red(
-				"Error"
-			)} ${chalk.gray("▪")} ${chalk.redBright(text)}`
+			`${chalk.cyan(this.date)}${chalk.gray(` - `)}${chalk.blue("[")}${chalk.cyanBright(`${this.name}`)}${chalk.blue("]")} ${chalk.red("Error")} ${chalk.gray("▪")} ${chalk.redBright(text)}`
 		);
+		if(this.writeLogs === false) return;
+		const formattedMessage = `${this.date} - [${this.name}] Error ▪ ${text}`
+		fs.appendFile(this.filepath, formattedMessage + '\n', (err) => {
+			if (err) {
+				throw new Error(err)
+			}
+		});
 	}
 	/**
-	 *
+	 * 
 	 * @param {String} text The text to log without colors.
 	 */
 	noColorsInfo(text) {
-		var d = new Date();
-		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
-		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
-
-		let date = date24;
-
-		if (this.format === 12) date = date12;
-		if (this.format === 24) date = date24;
-		console.log(`${date} - [${this.name}] Info ▪ ${text}`);
+		console.log(`${this.date} - [${this.name}] Info ▪ ${text}`);
+		if(this.writeLogs === false) return;
+		const formattedMessage = `${this.date} - [${this.name}] Info ▪ ${text}`
+		fs.appendFile(this.filepath, formattedMessage + '\n', (err) => {
+			if (err) {
+				throw new Error(err)
+			}
+		});
 	}
 	/**
-	 *
+	 * 
 	 * @param {String} text The text to log without colors.
 	 */
-	noColorsWarn(text) {
-		var d = new Date();
-		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
-		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
-
-		let date = date24;
-
-		if (this.format === 12) date = date12;
-		if (this.format === 24) date = date24;
-		console.log(`${date} - [${this.name}] Warn ▪ ${text}`);
+	 noColorsWarn(text) {
+		console.log(`${this.date} - [${this.name}] Warn ▪ ${text}`);
+		if(this.writeLogs === false) return;
+		const formattedMessage = `${this.date} - [${this.name}] Warn ▪ ${text}`
+		fs.appendFile(this.filepath, formattedMessage + '\n', (err) => {
+			if (err) {
+				throw new Error(err)
+			}
+		});
 	}
 	/**
-	 *
+	 * 
 	 * @param {String} text The text to log without colors.
 	 */
-	noColorsError(text) {
-		var d = new Date();
-		var date24 = moment(d).tz(String(this.timezone)).format("HH:mm:ss"); // 24 hour format
-		var date12 = moment(d).tz(String(this.timezone)).format("hh:mm:ss A"); // 12 hour format
-
-		let date = date24;
-
-		if (this.format === 12) date = date12;
-		if (this.format === 24) date = date24;
-		console.log(`${date} - [${this.name}] Error ▪ ${text}`);
+	 noColorsError(text) {
+		console.log(`${this.date} - [${this.name}] Error ▪ ${text}`);
+		if(this.writeLogs === false) return;
+		const formattedMessage = `${this.date} - [${this.name}] Error ▪ ${text}`
+		fs.appendFile(this.filepath, formattedMessage + '\n', (err) => {
+			if (err) {
+				throw new Error(err)
+			}
+		});
 	}
+
 }
 
 //exporting
